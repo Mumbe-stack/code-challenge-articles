3c32fc0a8c69161a2456a6cc8799f2c7d7bb58f5
Mercy <mercymumbe17@gmail.com>
2025-05-26 12:45:57 +0300

    added save method

--- lib/models/author.py
+++ lib/models/author.py
@@ -2,4 +2,18 @@ class Author:
      def __init__(self, name, id=None):
         self.id = id
         self.name = name
+        
+     def save(self):
+         conn = get_connection()
+         cursor = conn.cursor()
+         if self.id:
+             cursor.execute("UPDATE authors SET name = ? WHERE id = ?", (self.name, self.id))
+         else:
+             cursor.execute("INSERT INTO authors (name) VALUES (?)", (self.name,))
+             cursor.execute("SELECT last_insert_rowid()")
+             self.id = cursor.fetchone()[0]
+             conn.commit()
+             conn.close()
+        
+    
    
